8df185c77a4bf9b240b8d3d231b564cefe0256c3
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-02-02 19:31:40 -0800

    008: Strokes and fills (#7)

--- test/color/setting_test.dart
+++ test/color/setting_test.dart
@@ -78,6 +78,130 @@ void main() {
             'color_setting_background-setup-and'
             '-draw');
       });
+
+      testGoldens('fill(): example 1', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s.fill(color: const Color(0xFF969696));
+                s.circle(
+                  center: Offset(56, 46),
+                  diameter: 55,
+                );
+              },
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(tester, 'color_setting_fill-example-1');
+      });
+
+      testGoldens('fill(): example 2', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s.fill(color: const Color(0xFFCC6600));
+                s.circle(
+                  center: Offset(56, 46),
+                  diameter: 55,
+                );
+              },
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(tester, 'color_setting_fill-example-2');
+      });
+
+      testGoldens('noFill(): example 1', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s.circle(
+                  center: Offset(40, 40),
+                  diameter: 55,
+                );
+                s.noFill();
+                s.circle(
+                  center: Offset(60, 60),
+                  diameter: 55,
+                );
+              },
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(tester, 'color_setting_nofill-example-1');
+      });
+
+      testGoldens('stroke(): example 1', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s.stroke(color: const Color(0xFFAAAAAA));
+                s.circle(
+                  center: Offset(56, 46),
+                  diameter: 55,
+                );
+              },
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(tester, 'color_setting_stroke-example-1');
+      });
+
+      testGoldens('stroke(): example 2', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s.stroke(color: const Color(0xFFCC6600));
+                s.circle(
+                  center: Offset(56, 46),
+                  diameter: 55,
+                );
+              },
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(tester, 'color_setting_stroke-example-2');
+      });
+
+      testGoldens('noStroke(): example 1', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s.noStroke();
+                s.circle(
+                  center: Offset(56, 46),
+                  diameter: 55,
+                );
+              },
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(tester, 'color_setting_nostroke-example-1');
+      });
     });
   });
 }
