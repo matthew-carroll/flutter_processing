de23641953005b15e69af0da668b1e57ad023967
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-02-02 17:55:48 -0800

    005: Simplified Sketch constructor to accept callbacks. (#4)

--- test/core_test.dart
+++ test/core_test.dart
@@ -3,16 +3,20 @@ import 'package:flutter_processing/flutter_processing.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
+import 'test_infra.dart';
+
 void main() {
   group('core', () {
     testGoldens('user can paint background in setup()', (tester) async {
-      tester.binding.window
-        ..physicalSizeTestValue = Size(100, 100)
-        ..devicePixelRatioTestValue = 1.0;
+      configureWindowForSpecTest(tester);
 
       await tester.pumpWidget(
         Processing(
-          sketch: PaintBackgroundInSetupSketch(),
+          sketch: Sketch.simple(
+            setup: (s) {
+              s.background(color: const Color(0xFF404040));
+            },
+          ),
         ),
       );
 
@@ -20,13 +24,15 @@ void main() {
     });
 
     testGoldens('user can paint background in draw()', (tester) async {
-      tester.binding.window
-        ..physicalSizeTestValue = Size(100, 100)
-        ..devicePixelRatioTestValue = 1.0;
+      configureWindowForSpecTest(tester);
 
       await tester.pumpWidget(
         Processing(
-          sketch: PaintBackgroundInDrawSketch(),
+          sketch: Sketch.simple(
+            draw: (s) {
+              s.background(color: const Color(0xFF404040));
+            },
+          ),
         ),
       );
 
@@ -34,13 +40,15 @@ void main() {
     });
 
     testGoldens('user can paint orange background', (tester) async {
-      tester.binding.window
-        ..physicalSizeTestValue = Size(100, 100)
-        ..devicePixelRatioTestValue = 1.0;
+      configureWindowForSpecTest(tester);
 
       await tester.pumpWidget(
         Processing(
-          sketch: PaintOrangeBackgroundSketch(),
+          sketch: Sketch.simple(
+            draw: (s) {
+              s.background(color: const Color(0xFFFFCC00));
+            },
+          ),
         ),
       );
 
@@ -49,50 +57,22 @@ void main() {
 
     testGoldens('background in draw() replaces background in setup()',
         (tester) async {
-      tester.binding.window
-        ..physicalSizeTestValue = Size(100, 100)
-        ..devicePixelRatioTestValue = 1.0;
+      configureWindowForSpecTest(tester);
 
       await tester.pumpWidget(
         Processing(
-          sketch: PaintBackgroundInSetupAndDrawSketch(),
+          sketch: Sketch.simple(
+            setup: (s) {
+              s.background(color: const Color(0xFFFF0000));
+            },
+            draw: (s) {
+              s.background(color: const Color(0xFF404040));
+            },
+          ),
         ),
       );
 
       await screenMatchesGolden(tester, 'core_background_setup_draw');
     });
   });
-}
-
-class PaintBackgroundInSetupSketch extends Sketch {
-  @override
-  void setup() {
-    background(color: const Color(0xFF404040));
-  }
-}
-
-class PaintBackgroundInDrawSketch extends Sketch {
-  @override
-  void draw() {
-    background(color: const Color(0xFF404040));
-  }
-}
-
-class PaintOrangeBackgroundSketch extends Sketch {
-  @override
-  void draw() {
-    background(color: const Color(0xFFFFCC00));
-  }
-}
-
-class PaintBackgroundInSetupAndDrawSketch extends Sketch {
-  @override
-  void setup() {
-    background(color: const Color(0xFFFF0000));
-  }
-
-  @override
-  void draw() {
-    background(color: const Color(0xFF404040));
-  }
 }
