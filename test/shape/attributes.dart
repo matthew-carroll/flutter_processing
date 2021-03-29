6c18f743e4e36f2fc31fed337b987b31cfef5eaa
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-03-28 21:25:08 -0700

    026: strokeWeight

---
+++ test/shape/attributes.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_processing/flutter_processing.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+import '../test_infra.dart';
+
+void main() {
+  group('Shape', () {
+    group('Attributes', () {
+      testGoldens('strokeWeight(): example 1', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s
+                  ..noLoop()
+                  ..strokeWeight(1)
+                  ..line(Offset(20, 20), Offset(80, 20))
+                  ..strokeWeight(4)
+                  ..line(Offset(20, 40), Offset(80, 40))
+                  ..strokeWeight(10)
+                  ..line(Offset(20, 70), Offset(80, 70));
+              },
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(tester, 'shape_attributes_stroke-weight-example-1');
+      });
+
+      testWidgets('strokeWeight() invalid value', (tester) async {
+        configureWindowForSpecTest(tester);
+
+        await expectLater(() {
+          final sketch = Sketch.simple();
+
+          sketch.strokeWeight(-1);
+        }, throwsA(isA<Exception>()));
+      });
+    });
+  });
+}
