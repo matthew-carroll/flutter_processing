5e771bb6fc2009fd071007ceb0fbb0e21782cdaf
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-05-01 21:46:44 -0700

    029: Mouse input

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,14 +30,28 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.yellow,
       body: Center(
         child: Processing(
-          sketch: Sketch.simple(
-            setup: (s) {
-              // TODO:
-            },
-            draw: (s) {
-              // TODO:
-            },
-          ),
+          sketch: Sketch.simple(setup: (s) {
+            s.size(width: 500, height: 500);
+          }, draw: (s) {
+            // TODO:
+          }, mouseMoved: (s) {
+            print(
+                'mouseMoved - current position: ${s.mouseX}, ${s.mouseY}, previous position: ${s.pmouseX}, ${s.pmouseY}');
+          }, mousePressed: (s) {
+            print(
+                'mousePressed - current position: ${s.mouseX}, ${s.mouseY}, previous position: ${s.pmouseX}, ${s.pmouseY}');
+          }, mouseDragged: (s) {
+            print(
+                'mouseDragged - current position: ${s.mouseX}, ${s.mouseY}, previous position: ${s.pmouseX}, ${s.pmouseY}');
+          }, mouseReleased: (s) {
+            print(
+                'mouseReleased - current position: ${s.mouseX}, ${s.mouseY}, previous position: ${s.pmouseX}, ${s.pmouseY}');
+          }, mouseClicked: (s) {
+            print(
+                'mouseClicked - current position: ${s.mouseX}, ${s.mouseY}, previous position: ${s.pmouseX}, ${s.pmouseY}');
+          }, mouseWheel: (s, count) {
+            print('mouseWheel - count: $count');
+          }),
         ),
       ),
     );
