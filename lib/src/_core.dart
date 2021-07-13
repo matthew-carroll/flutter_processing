fb45d69ffe9fa5d455757521c532fad6833c9f52
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-07-12 23:57:41 -0700

    43: save()

--- lib/src/_core.dart
+++ lib/src/_core.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
 import 'dart:ui';
@@ -7,6 +8,8 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart' hide Image;
+import 'package:path/path.dart' as path;
+import 'package:image/image.dart' as imageFormats;
 
 class Processing extends StatefulWidget {
   const Processing({
@@ -1085,6 +1088,67 @@ class Sketch {
   LogicalKeyboardKey? _key;
   LogicalKeyboardKey? get key => _key;
 
+  //------- Start Output/Image -----
+  Future<void> save({
+    required File file,
+    ImageFileFormat? format,
+  }) async {
+    late ImageFileFormat imageFormat;
+    if (format != null) {
+      imageFormat = format;
+    } else {
+      final imageFormatFromFile = _getImageFileFormatFromFilePath(file.path);
+      if (imageFormatFromFile == null) {
+        throw Exception('Cannot save image to file with invalid extension and no explicit image type: ${file.path}');
+      }
+      imageFormat = imageFormatFromFile;
+    }
+
+    // Retrieve the pixel data for the current sketch painting.
+    final rawImageData = pixels.buffer.asUint8List();
+
+    // Convert the pixel data to the desired format.
+    late List<int> formattedImageData;
+    switch (imageFormat) {
+      case ImageFileFormat.png:
+        formattedImageData = imageFormats.encodePng(
+          imageFormats.Image.fromBytes(width, height, rawImageData),
+        );
+        break;
+      case ImageFileFormat.jpeg:
+        formattedImageData = imageFormats.encodeJpg(
+          imageFormats.Image.fromBytes(width, height, rawImageData),
+        );
+        break;
+      case ImageFileFormat.tiff:
+        throw UnimplementedError('Tiff images are not supported in save()');
+      case ImageFileFormat.targa:
+        formattedImageData = imageFormats.encodeTga(
+          imageFormats.Image.fromBytes(width, height, rawImageData),
+        );
+        break;
+    }
+
+    // Write the formatted pixels to the given file.
+    await file.writeAsBytes(formattedImageData);
+  }
+
+  ImageFileFormat? _getImageFileFormatFromFilePath(String filePath) {
+    final fileExtension = path.extension(filePath);
+    switch (fileExtension) {
+      case '.png':
+        return ImageFileFormat.png;
+      case '.jpg':
+        return ImageFileFormat.jpeg;
+      case '.tif':
+        return ImageFileFormat.tiff;
+      case '.tga':
+        return ImageFileFormat.targa;
+      default:
+        return null;
+    }
+  }
+
   //------- Start Transform ------
   void translate({
     double? x,
@@ -1185,4 +1249,11 @@ enum ArcMode {
   open,
   chord,
   pie,
+}
+
+enum ImageFileFormat {
+  png,
+  jpeg,
+  tiff,
+  targa,
 }
