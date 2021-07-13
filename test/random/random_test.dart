625f34eb1aad5efd98124792ac7da09669139975
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-07-12 23:13:10 -0700

    42: randomGaussian() (#46)

--- test/random/random_test.dart
+++ test/random/random_test.dart
@@ -1,5 +1,12 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_processing/flutter_processing.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+import '../test_infra.dart';
 
 void main() {
   group('Random', () {
@@ -28,6 +35,60 @@ void main() {
           ),
         ),
       );
+    });
+
+    testGoldens('randomGaussian() - vertical distribution', (tester) async {
+      configureWindowForSpecTest(tester);
+
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            draw: (s) async {
+              s
+                ..noLoop()
+                ..randomSeed(0);
+
+              for (int y = 0; y < 100; y++) {
+                final x = s.randomGaussian() * 15;
+                s.line(Offset(50, y.toDouble()), Offset(50.0 + x, y.toDouble()));
+              }
+            },
+          ),
+        ),
+      );
+
+      await screenMatchesGolden(tester, 'random_gaussian_example-1');
+    });
+
+    testGoldens('randomGaussian() - radial distribution', (tester) async {
+      configureWindowForSpecTest(tester);
+
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            setup: (s) async {},
+            draw: (s) async {
+              s
+                ..noLoop()
+                ..randomSeed(0)
+                ..translate(x: s.width / 2, y: s.height / 2)
+                ..stroke(color: Colors.black);
+
+              final distribution = List<double>.generate(360, (index) {
+                return s.randomGaussian() * 15;
+              });
+
+              for (int i = 0; i < distribution.length; i++) {
+                s.rotate(2 * pi / distribution.length);
+                final dist = distribution[i].abs();
+                s.line(Offset(0, 0), Offset(dist, 0));
+              }
+            },
+          ),
+        ),
+      );
+
+      await screenMatchesGolden(tester, 'random_gaussian_example-2');
     });
   });
 }
